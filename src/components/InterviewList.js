51d68529b9330010268aa11e2e4b8832d586986a
flavian mohanraj <flavianmohanraj@gmail.com>
2021-02-20 23:58:02 -0500

    styling and refactoring interviewlist

--- src/components/InterviewList.js
+++ src/components/InterviewList.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewList.scss"
 
+export default function InterviewList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    return <InterviewerListItem 
+      number = {interviewer.id}
+      setInterviewer={props.setInterviewer}
+    />;
+  });
 
-export default function InterviewList(props){
-
-  return(
+  return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
-  )
+  );
 }
