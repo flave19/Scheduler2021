8e4380f187085ea315733908f59e910b7d82082d
flavian mohanraj <flavianmohanraj@gmail.com>
2021-02-21 20:25:26 -0500

    refactoring interviewer components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,15 +4,13 @@ let classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
   let interviewClass = classNames("interviewers__item", {
-    // 'interviewers__item--selected-image':(props.selected && props.avatar),
-    // 'interviewers__item-image':props.avatar,
     'interviewers__item--selected':props.selected,
   })
 
   return (
     <li 
     className={interviewClass}
-    onClick={() => props.setInterviewer(props.name)} >
+    onClick={props.setInterviewer} >
       <img
         className="interviewers__item-image"
         src={props.avatar}
