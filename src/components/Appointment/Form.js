d05da47182f38ad58b160ae3ff4970687387e2e2
flavian mohanraj <flavianmohanraj@gmail.com>
2021-02-24 22:12:00 -0500

    adding state to appointment form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -2,32 +2,48 @@ import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  function cancel(){
+    reset()
+    props.onCancel()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            value={name}
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-        */
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={interviewer}
-          setInterviewer={setInterviewer}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button onClick={() => cancel()} danger>
+            Cancel
+          </Button>
+          <Button onSubmit={(event) => event.preventDefault()} confirm>
+            Save
+          </Button>
         </section>
       </section>
     </main>
